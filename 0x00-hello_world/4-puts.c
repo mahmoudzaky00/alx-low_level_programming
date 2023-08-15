dd11c978417781306ceccceb5d2ad2ca0ac114f9
mahmoudzaky00 <mahnoudzaky841@gmail.com>
2023-08-14 17:01:34 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - entry poit
- * return: always 0 (success)
+ * main - this is the main function
+ *
+ * Return: always 0
  */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
