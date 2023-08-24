6199927a88ea3903cbc88ed3b2bfa6edde8e6e61
mahmoudzaky00 <mahnoudzaky841@gmail.com>
2023-08-24 15:21:01 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,7 +22,7 @@ char *_strncat(char *dest, char *src, int n)
 	 * if it contains n or more bytes
 	 */
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + 1] = src[i];
+		dest[c + i] = src[i];
 	/*null terminate dest*/
 	dest[c + i] = '\0';
 
