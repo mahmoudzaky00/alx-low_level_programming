4d12d7d7a0db01de9b5f9798180aebec74014572
mahmoudzaky00 <mahnoudzaky841@gmail.com>
2023-09-13 16:33:11 -0700

    a

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 	int b;
 
 	if (argc != 2)
-		printf("error\n"), exit(1);
+		printf("Error\n"), exit(1);
 	b = atoi(argv[1]);
 	if (b < 0)
 		printf("Error\n"), exit(2);
